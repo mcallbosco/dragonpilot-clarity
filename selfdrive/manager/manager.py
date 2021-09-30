40e6120a0276ae9476873008f21a621390f09a7a
alfhern <hz.alfonso@gmail.com>
2021-09-30 14:07:12 +0200

    VisionTurnController Implementation

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -41,6 +41,7 @@ def manager_init() -> None:
     ("HasAcceptedTerms", "0"),
     ("HandsOnWheelMonitoring", "0"),
     ("OpenpilotEnabledToggle", "1"),
+    ("TurnVisionControl", "1"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
