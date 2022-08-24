e69b08c7c4e2cc396faec5436053a38986fb4d0b
vanillagorillaa <ntmccoy@yahoo.com>
2022-08-23 18:52:21 -0700

    0.9.5 Clarity

--- selfdrive/car/honda/fingerprints.py
+++ selfdrive/car/honda/fingerprints.py
@@ -10,6 +10,30 @@ Ecu = car.CarParams.Ecu
 
 
 FW_VERSIONS = {
+  CAR.CLARITY: {
+    (Ecu.shiftByWire, 0x18da0bf1, None): [
+        b'54008-TRW-A910\x00\x00',
+    ],
+    (Ecu.vsa, 0x18da28f1, None): [
+        b'57114-TRW-A010\x00\x00',
+        b'57114-TRW-A020\x00\x00',
+    ],
+    (Ecu.eps, 0x18da30f1, None): [
+        b'39990-TRW-A020\x00\x00',
+        b'39990-TRW,A020\x00\x00', #modified firmware
+    ],
+    (Ecu.srs, 0x18da53f1, None): [
+        b'77959-TRW-A210\x00\x00',
+        b'77959-TRW-A220\x00\x00',
+    ],
+    (Ecu.gateway, 0x18daeff1, None): [
+        b'38897-TRW-A010\x00\x00',
+    ],
+    (Ecu.combinationMeter, 0x18da60f1, None): [
+        b'78109-TRW-A020\x00\x00',
+        b'78109-TRW-A030\x00\x00',
+    ],
+  },
   CAR.ACCORD: {
     (Ecu.programmedFuelInjection, 0x18da10f1, None): [
       b'37805-6A0-8720\x00\x00',
